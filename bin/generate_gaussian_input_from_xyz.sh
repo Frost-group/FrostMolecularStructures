9b1340576aa5e4d64fed0e06ba73436818b14fc1
Jarvist Moore Frost <jarvist@gmail.com>
2025-07-29 18:50:51 +0100

    Jarv actually remembers how to write Bash.

--- bin/generate_gaussian_input_from_xyz.sh
+++ bin/generate_gaussian_input_from_xyz.sh
@@ -1,20 +1,20 @@
 METHOD="pbe1pbe"
 BASIS="ccpvtz"
 
-HEADER= << EOF
+for f
+do 
+    filename="${f%.*}_${METHOD}-${BASIS}.gjf"
+
+cat > $filename << EOF
 %nprocshared=32
 %mem=100GB
-# p opt=tight Int=UltraFine ${METHOD}/{BASIS}
+# p opt=tight Int=UltraFine ${METHOD}/${BASIS}
 
 Autojob.
 
 0 1
 EOF
 
-for f
-do 
-    filename="${f%.*}_${METHOD}-${BAS    IS}.gjf"
-    echo $HEADER > $filename
     cat $f >> $filename
     echo >> $filename
 done
